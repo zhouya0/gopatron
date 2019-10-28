90a12c13987ed3c1462482a3cd06b3905bde616b
zhouya0 <yao.zhoupolytech@gmail.com>
2019-10-28 09:14:57 +0800

    Add Decorator small demo

---
+++ decorator/httpDecorator.go
@@ -0,0 +1,34 @@
+package httpDecorator
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+//装饰器模式，在go里面，看来一般就是指函数的封装。
+
+func WithServerHeader(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("--->WithServerHeader()")
+		w.Header().Set("Server", "HelloServer v0.0.1")
+		h(w, r)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Recieved Request %s from %s\n", r.URL.Path, r.RemoteAddr)
+	fmt.Fprintf(w, "Hello, World!" + r.URL,PAth)
+}
+
+
+func serve() {
+	http.HandleFunc("/v1/hello", WithServerHeader(hello))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+
