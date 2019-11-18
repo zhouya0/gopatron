51475a752c1515a0356a0c3a8d42e5bab7915fe4
zhouya0 <yao.zhoupolytech@gmail.com>
2019-11-18 10:19:30 +0800

    Add visotor test

--- visitor/visitor.go
+++ visitor/visitor.go
@@ -5,30 +5,32 @@ import (
 )
 
 type IElement interface {
-	Accept(visitor IVisitor)
+	Accept(visitor IVisitor) string
 }
 
 type Element struct {
 }
 
-func (e Element) Accept(visitor IVisitor) {
-	visitor.visit()
+func (e Element) Accept(visitor IVisitor) string {
+	return visitor.visit()
 }
 
 type IVisitor interface {
-	visit()
+	visit() string
 }
 
 type ProductionVisitor struct {
 }
 
-func (p ProductionVisitor) visit() {
+func (p ProductionVisitor) visit() string {
 	fmt.Println("This is the ProductionVisitor!")
+	return "ProductionVisitor"
 }
 
 type TestingVisitor struct {
 }
 
-func (t TestingVisitor) visit() {
+func (t TestingVisitor) visit() string {
 	fmt.Println("This is the testingVisitor")
+	return "TestingVisitor"
 }
--- visitor/visitor_test.go
+++ visitor/visitor_test.go
@@ -5,5 +5,12 @@ import (
 )
 
 func TestVisitor(t *testing.T) {
-	//TODO
+	e := Element{}
+	p := ProductionVisitor{}
+	q := TestingVisitor{}
+	es := e.Accept(p)
+	qs := e.Accept(q)
+	if es != "ProductionVisitor" || qs != "TestingVisitor" {
+		t.Error("Not returning the right type!")
+	}
 }
