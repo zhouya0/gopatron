60744d73c4fac017269abfb378fec40218efc5d3
zhouya0 <yao.zhoupolytech@gmail.com>
2020-02-25 09:31:51 +0800

    Review observer

--- observer/observer_test.go
+++ observer/observer_test.go
@@ -4,15 +4,22 @@ import (
 	"testing"
 )
 
+func InputObeserver(b Obeserver) {
+	b.Update(nil)
+}
+
 func testObserver(t testing.T) {
-	c1 := &ConcreteObserver{1}
-	c2 := &ConcreteObserver{2}
-	// 注意这里必须c1,c2必须是指针，不然就会出现“has pointer receiver的错”
-	// 其实道理就是，我的实现是指针，我传只能传指针；我的实现如果是实体，我传，可以传实体，可以传指针。
-	// 指针能找到实体，但是实体找不到指针！！！
+	c1 := ConcreteObserver{1}
+	c2 := ConcreteObserver{2}
 	var subject ConcreteSubject
-	subject.Attach(c1)
-	subject.Attach(c2)
+	// 谁实现了Update方法？是ConcreteObserver还是&ConcreteObserver？
+	// subject.Attach(c1) cannot use c1 (variable of type ConcreteObserver) as Obeserver value in argument to subject.Attach: missing method Update
+	// 接收体定义了到底是指针对象还是非指针对象来调用方法
+	subject.Attach(&c1)
+	subject.Attach(&c2)
 	event := Event{"this is a event!"}
+	// 这里为什么成功？是不是它做了一个自动的转化？
+	c1.Update(&event)
+	(&c1).Update(&event)
 	subject.Notify(&event)
 }
